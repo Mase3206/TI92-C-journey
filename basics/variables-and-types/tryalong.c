a97ce25741aef2625efd879488b82d82f5b1b752
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-01-28 16:03:12 -0700

    variables and types are done

---
+++ basics/variables-and-types/tryalong.c
@@ -0,0 +1,20 @@
+// here i'll try out some code as the website discusses it
+
+#include <stdio.h>
+
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+
+
+int main() {
+	int foo;
+	int bar = 1;
+
+	int a = 0, b = 1, c = 2, d = 3, e = 4;
+	a = b - c + d * e;
+
+	printf("hi there");
+	printf("%d", a);
+	return 0;
+}
